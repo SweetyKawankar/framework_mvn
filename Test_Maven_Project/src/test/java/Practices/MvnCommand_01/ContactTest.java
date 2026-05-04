e33b0bc4f4246dd61538532f0a4e4c5e68a10b14
Swapnil <Swapnil@SwapnilK>
2026-05-04 11:28:55 +0530

    Framework_MVN_Jenkins

---
+++ Test_Maven_Project/src/test/java/Practices/MvnCommand_01/ContactTest.java
@@ -0,0 +1,25 @@
+package Practices.MvnCommand_01;
+
+import org.testng.annotations.Test;
+
+public class ContactTest {
+	
+	@Test
+	public void createcontactTest() {
+		String URL = System.getProperty("url");
+		String BROWSER = System.getProperty("browser");
+		String USERNAME = System.getProperty("username");
+		String PASSWORD = System.getProperty("password");
+		
+		System.out.println(URL);
+		System.out.println(BROWSER);
+		System.out.println(USERNAME);
+		System.out.println(PASSWORD);
+		System.out.println("Execute createcontactTest");
+	}
+
+	@Test
+	public void modifycontactTest() {
+		System.out.println("Execute modifycontactTest");
+	}
+}
---
+++ Test_Maven_Project/src/test/java/Practices/MvnCommand_02/OrgTest.java
@@ -0,0 +1,17 @@
+package Practices.MvnCommand_02;
+
+import org.testng.annotations.Test;
+
+public class OrgTest {
+	
+		@Test
+		public void createOrgTest() {
+			System.out.println("Execute createcontactTest");
+		}
+
+		@Test
+		public void modifyOrgTest() {
+			System.out.println("Execute modifycontactTest");
+		}
+
+}
